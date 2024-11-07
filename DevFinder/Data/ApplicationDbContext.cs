c1284dbfc5a59d29a70543044ee72c84718bde31
max gromov <maximgromov@gmail.com>
2024-11-07 13:41:45 +0200

    Added Migrations

--- DevFinder/Data/ApplicationDbContext.cs
+++ DevFinder/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DevFinder.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DevFinder.Data
 {
     public class ApplicationDbContext: IdentityDbContext
     {
+        public DbSet<JobPosting> JobPostings { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
         {
             
